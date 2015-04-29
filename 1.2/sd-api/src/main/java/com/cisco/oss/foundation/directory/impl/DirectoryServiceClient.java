89f92a8075ef45322b139c74c76050dc35518b15
xuanzhong <xuzhong@cisco.com>
2015-04-29 15:13:52 -0400

    fix the backward compatibility issue.

--- 1.2/sd-api/src/main/java/com/cisco/oss/foundation/directory/impl/DirectoryServiceClient.java
+++ 1.2/sd-api/src/main/java/com/cisco/oss/foundation/directory/impl/DirectoryServiceClient.java
@@ -3,7 +3,7 @@ package com.cisco.oss.foundation.directory.impl;
 import com.cisco.oss.foundation.directory.client.DirectoryServiceRestfulClient;
 
 /**
- * The class is only for old compatible
+ * The class is only for backward compatibility
  * @deprecated
  */
 public class DirectoryServiceClient {
@@ -12,24 +12,24 @@ public class DirectoryServiceClient {
      * use {@link DirectoryServiceRestfulClient#SD_API_SD_SERVER_FQDN_PROPERTY}
      */
     @Deprecated
-    String SD_API_SD_SERVER_FQDN_PROPERTY = DirectoryServiceRestfulClient.SD_API_SD_SERVER_FQDN_PROPERTY;
+    public static String SD_API_SD_SERVER_FQDN_PROPERTY = DirectoryServiceRestfulClient.SD_API_SD_SERVER_FQDN_PROPERTY;
 
     /**
      * use {@link DirectoryServiceRestfulClient#SD_API_SD_SERVER_FQDN_DEFAULT}
      */
     @Deprecated
-    String SD_API_SD_SERVER_FQDN_DEFAULT = DirectoryServiceRestfulClient.SD_API_SD_SERVER_FQDN_DEFAULT;
+    public static String SD_API_SD_SERVER_FQDN_DEFAULT = DirectoryServiceRestfulClient.SD_API_SD_SERVER_FQDN_DEFAULT;
 
     /**
      * use {@link DirectoryServiceRestfulClient#SD_API_SD_SERVER_PORT_PROPERTY}
      */
     @Deprecated
-    String SD_API_SD_SERVER_PORT_PROPERTY = DirectoryServiceRestfulClient.SD_API_SD_SERVER_PORT_PROPERTY;
+    public static String SD_API_SD_SERVER_PORT_PROPERTY = DirectoryServiceRestfulClient.SD_API_SD_SERVER_PORT_PROPERTY;
 
     /**
      * use {@link DirectoryServiceRestfulClient#SD_API_SD_SERVER_PORT_DEFAULT}
      */
     @Deprecated
-    int SD_API_SD_SERVER_PORT_DEFAULT = DirectoryServiceRestfulClient.SD_API_SD_SERVER_PORT_DEFAULT;
+    public static int SD_API_SD_SERVER_PORT_DEFAULT = DirectoryServiceRestfulClient.SD_API_SD_SERVER_PORT_DEFAULT;
 
 }
